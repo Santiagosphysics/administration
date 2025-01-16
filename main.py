a99cf145724838bbeda10bb9017333bab823956f
Santiagosphysics <msjimenez@unbosque.edu.co>
2025-01-16 12:05:08 -0500

    1.0.2

--- main.py
+++ main.py
@@ -1,13 +1,120 @@
-import os 
-from utils import options
+from utils import sort_cont, new_item, del_item, dict_procces
 import tkinter as tk 
 
+import tkinter as tk
+from utils import sort_cont, new_item, del_item
+
+def handle_input(event):
+    list_names = sort_cont()
+    procces = dict_procces()
+    
+
+
+
+
+
+
+    input_text = input_field.get().strip()
+    input_field.delete(0, tk.END)
+
+    if input_text.lower() in ['exit', 'e']:
+        app.quit()
+    else:
+        try:
+            choice = int(input_text)
+            if 1 <= choice <= len(list_names):
+                dict_1 = {i+1:list_names[i] for i in range(len(list_names))}
+                choice = dict_1[choice]
+                print(procces[choice])
+
+                for option in list_names:
+                    if choice == option:
+                        display_message(procces[choice])
+
+
+
+                    #display_response(f"You selected: {list_names[choice - 1]}")
+
+                    #display_response("If you want to add another option, write 1.\n"
+                    #                 "If you want to delete an option, write 0.\n"
+                    #                 "If you want to go to the menu, write m.")                    
+                
+                    #input_field.bind('<Return>', handle_secondary_input)  # Bind next input to a new handler
+
+            else:
+                display_response("Invalid choice. Please select a valid option number.")
+
+        except ValueError:
+            display_response("Invalid input. Please enter a number or type 'exit' to quit.")
+
+            
+
+def handle_secondary_input(event):
+    input_text_2 = input_field.get().strip()
+    input_field.delete(0, tk.END)
+
+    if input_text_2.lower() in ['menu', 'm']:
+        display_menu()
+    else:
+        try:
+            choice_2 = int(input_text_2)
+
+            if choice_2 == 1:
+                display_message("Please enter the new option name:")
+                input_field.bind('<Return>', handle_new_item)
+
+            elif choice_2 == 0:
+                display_message("Please enter the name of the option to delete:")
+                input_field.bind('<Return>', handle_delete_item)
+
+            else:
+                display_response("Invalid input. Please enter 1, 0, or 'menu'.")
+
+        except ValueError:
+            display_response("Invalid input. Please enter a valid option or type 'menu'.")
+
+def handle_new_item(event):
+    input_text_3 = input_field.get().strip()
+    input_field.delete(0, tk.END)
+    new_1 = new_item(input_text_3)
+    display_message(f"You have created the option '{input_text_3}'.")
+    display_message(f"Current options: \n{new_1}")
+    display_menu()
+
+def handle_delete_item(event):
+    input_text_3 = input_field.get().strip()
+    input_field.delete(0, tk.END)
+    new_1 = del_item(rem_item=input_text_3)
+    display_message(f"You have deleted the option '{input_text_3}'.")
+    display_message(f"Current options: \n{new_1}")
+    display_menu()
+
+def display_menu():
+    list_names = sort_cont()
+    output_text.set("Routes of access:\n" + "\n".join([f"{1+i}. {name}" for i, name in enumerate(list_names)]))
+    input_field.focus()
+
 def display_response(message):
-    output_text.set(message + "\nType a number to choose an option or type 'exit' to quit")
-    
+    output_text.set(message + "\n\nType a number to choose an option or type 'exit' to quit")
 
+def display_message(message):
+    output_text.set(message)
 
+app = tk.Tk()
+app.title("Administrator")
+app.geometry("1500x900")
+app.configure(bg='black')
 
-def handle_input(opt):
-    
-    
+output_text = tk.StringVar()
+output_text.set("")
+
+output_label = tk.Label(app, textvariable=output_text, fg="white", bg="black", font=("Courier", 12), justify="left")
+output_label.pack(pady=20, padx=20, anchor="w")
+
+input_field = tk.Entry(app, font=("Courier", 14), fg='white', bg='black', insertbackground='white', width=40)
+input_field.pack(pady=10)
+input_field.bind("<Return>", handle_input)
+
+display_menu()
+
+app.mainloop()
--- utils.py
+++ utils.py
@@ -1,13 +1,93 @@
-list_names = ['Custom Control', 'Picking', 'Payment of Public Service and Administration', 'Suppliers', 'Contact', 'Solutionary']
 
-def options(list_names):
-    options=[f'{i+1}. {list_names[i]}' for i in range(len(list_names))]
-    for option in options:
-        print(option)
-    return options
 
-# print(type(options(list_names=list_names)))
+def add_name(content, new_name):
+    #new_name = input('Write new item: ')
+    if new_name == '' or new_name == '  ':
+        return content
+    else:
+        content.append(new_name)
+        return content
+    
+def sort_cont():
+    with open('./names.txt') as file:
+        content = file.read()
+        content = content.strip().split(',')
+        content = [i.replace(' ', '') for i in content]
+        content = [i.replace("'", '') for i in content]
+    return content
 
-# op = options(list_names=list_names)
-# print(op)
+def new_item(new_name):
+    content = sort_cont()
+    content = add_name(content=content, new_name=new_name)
+    list_names = ", ".join(i for i in content)
 
+    with open('./names.txt', 'w') as file:
+        file.write(list_names)
+    return 'A new item has beed added'
+
+
+def del_item(rem_item):
+    #rem_item = input('Wirte the item to delete: ')
+    content = sort_cont()
+    content = ", ".join(i for i in content if i != rem_item)
+    
+    with open('./names.txt', 'w') as file:
+        file.write(content)
+    return f'The item: {rem_item} has been deleted'
+
+
+
+def dict_procces():
+    procces = {
+        'Custom_Control': 
+        '''Head of the area Ing. Edgar Ruíz 
+                            \n\nEl deposito que se encuentra frontalmente a la entrada de la bodega es el lugar donde actualmente Aviatour realiza 
+                            \nel proceso de nacionalización, allí mismo, a las 8 a.m. aproximadamente se abren las puertas en compañía de los 
+                            \nencargados, seguridad y personal de importaciones, es importante resaltar que no se puede abrir la puerta sin que 
+                            \ntodos se encuentren en el mismo lugar.
+
+                            \n\nEl personal del depósito se encarga de realizar el desembarque de los contenedores, teniendo en cuenta que a cada 
+                            \ncontenedor le pueden pertenecer diferentes facturas y su vez distinto número de cajas asociado a la factura.
+
+                            \n\nDesde el depósito empieza el proceso de carga de cajas, en la que se debe anotar el número de factura y cajas.
+
+                            \n\nUna vez la caja sale del depósito no puede volver a entrar, además, se realizan varios registros fotográficos
+                            \ndesde tres diferentes posiciones, esto con el fin de evidenciar las condiciones con las que son recibidas las cajas.
+
+                            \nA partir de este punto las cajas son llevadas a la zona 15
+        ''',
+        'Picking':
+        ''' Head of the area Ing. Pablo Rodríguez
+            \n\n
+        
+        ''',
+        'Zones':
+        '''
+            Zone 1: 
+            Zone 2: Modula\n
+            Zone 3: Repuestos pesados o grandes\n
+            Zone 4: Mesanine (Repuestos livianos, filtros, empaques...)\n
+            Zone 5: Líquidos\n
+            Zone 6: Farolas y bompers
+            Zone 7:
+            Zone 8:
+            Zone 9:
+            Zone 10:
+            Zone 11:
+            Zone 12
+            Zone 13: Backorder o entrega urgente
+            Zone 14:
+            Zone 15: Importaciones o recibos
+        '''
+
+        
+
+    }
+
+
+
+    return procces
+
+
+d = dict_procces()
+print(d)
